288928f9f4f8006dee08b0db5856febc224d55b4
apereial <apereial@emeal.nttdata.com>
2023-01-16 09:32:41 +0100

    Added Polymorphism

---
+++ Calculator Project/Polymorphism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Project.Polymorphism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            Students student = new Students();
+            student.Name = "Pepe";
+
+            //boxing n autoboxing
+            arrayList.Add(student);
+
+            Console.WriteLine(((Students)arrayList[0]).Name);
+
+            //arraylist[0].Name -----> Error
+
+            //segunda forma
+
+            object obj= new Students(); //boxing
+
+            ((Students)obj).Name = "Alberto";
+            Console.WriteLine(((Students)obj).Name);
+        }
+    }
+}
